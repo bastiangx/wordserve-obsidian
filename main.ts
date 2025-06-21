86579b3af00897a12ea39314da47d92144673c9d
bast <bast@Mac.mynet>
2025-06-21 08:40:47 +0100

    feat: decoupled all methods. main is now only aggregating

--- main.ts
+++ main.ts
@@ -1,40 +1,19 @@
-import { App, Notice, Plugin, PluginSettingTab, Setting } from "obsidian";
-import { TyprIPC } from "./src/typr-ipc";
-import { TyprSuggest } from "./src/typr-suggest";
-import { CONFIG } from "./src/config";
+import { Plugin } from "obsidian";
+import { TyperIPC } from "./src/core/client";
+import {  TyperSuggest } from "./src/ui/render";
+import { DEFAULT_SETTINGS } from "./src/core/config";
+import { TyperPluginSettings } from "./src/models/types";
+import { TyperSettingTab } from "src/settings/settings-tab";
 
-/**
- * User settings that can be changed in the settings tab
- */
-interface TyprPluginSettings {
-  minWordLength: number;
-  maxSuggestions: number;
-  fuzzyMatching: boolean;
-  debounceTime: number;
-  numberSelection: boolean;
-}
-
-/**
- * Default settings derived from centralized CONFIG
- */
-const DEFAULT_SETTINGS: TyprPluginSettings = {
-  minWordLength: CONFIG.plugin.minWordLength,
-  maxSuggestions: CONFIG.plugin.maxSuggestions,
-  fuzzyMatching: CONFIG.plugin.fuzzyMatching,
-  debounceTime: CONFIG.plugin.debounceTime,
-  numberSelection: CONFIG.plugin.numberSelection,
-};
-
-export default class TyprPlugin extends Plugin {
-  settings: TyprPluginSettings;
-  ipc: TyprIPC;
-  suggestor: TyprSuggest;
+export default class TyperPlugin extends Plugin {
+  settings: TyperPluginSettings;
+  ipc: TyperIPC;
+  suggestor: TyperSuggest;
   statusBarEl: HTMLElement;
 
   async onload() {
     await this.loadSettings();
 
-    // Update the suggestor settings when plugin settings change
     const updateSuggestorSettings = () => {
       this.suggestor.minChars = this.settings.minWordLength;
       this.suggestor.limit = this.settings.maxSuggestions;
@@ -43,77 +22,29 @@ export default class TyprPlugin extends Plugin {
       this.suggestor.debounceDelay = this.settings.debounceTime;
     };
 
-    // Initialize the IPC connection to backend
-    this.ipc = new TyprIPC(this);
+    this.ipc = new TyperIPC(this);
+    this.suggestor = new TyperSuggest(this.app, this.ipc);
 
-    // Create the suggestor
-    this.suggestor = new TyprSuggest(this.app, this.ipc);
-
-    // Apply initial settings to suggestor
     updateSuggestorSettings();
-
-    // Register the suggest functionality
     this.registerEditorSuggest(this.suggestor);
 
-    // Create an icon in the left ribbon
-    this.addRibbonIcon(
-      "keyboard",
-      "Typr Autocompletion",
-      async (evt: MouseEvent) => {
-        // Test the connection to backend
-        try {
-          const isReady = await this.ipc.initialize();
-          if (isReady) {
-            new Notice("Typr autocompletion is ready and connected");
-          } else {
-            new Notice("Failed to connect to Typr backend");
-          }
-        } catch (error) {
-          new Notice("Failed to initialize Typr: " + error);
-        }
-      }
-    );
-
-    // Add a status bar item
+    // statusBar shows current status
     this.statusBarEl = this.addStatusBarItem();
-    this.statusBarEl.setText("Typr: Ready");
+    this.statusBarEl.setText("Typer: Ready");
 
-    // Add a command to force-enable autocompletion
-    this.addCommand({
-      id: "force-enable-typr",
-      name: "Enable Typr autocompletion",
-      callback: async () => {
-        try {
-          const isReady = await this.ipc.initialize();
-          if (isReady) {
-            new Notice("Typr autocompletion enabled");
-            this.statusBarEl.setText("Typr: Active");
-          } else {
-            new Notice("Failed to enable Typr");
-            this.statusBarEl.setText("Typr: Error");
-          }
-        } catch (error) {
-          new Notice("Failed to initialize Typr: " + error);
-          this.statusBarEl.setText("Typr: Error");
-        }
-      },
-    });
+    this.addSettingTab(new TyperSettingTab(this.app, this));
 
-    // Add a settings tab
-    this.addSettingTab(new TyprSettingTab(this.app, this));
-
-    // Initialize the connection
     this.ipc
       .initialize()
       .then((isReady) => {
         if (isReady) {
-          this.statusBarEl.setText("Typr: Active");
+          this.statusBarEl.setText("Typer: Active");
         } else {
-          this.statusBarEl.setText("Typr: Inactive");
+          this.statusBarEl.setText("Typer: Inactive");
         }
       })
       .catch(() => {
-        this.statusBarEl.setText("Typr: Error");
+        this.statusBarEl.setText("Typer: Error");
       });
   }
 
@@ -127,99 +58,5 @@ export default class TyprPlugin extends Plugin {
 
   async saveSettings() {
     await this.saveData(this.settings);
-  }
-}
-
-class TyprSettingTab extends PluginSettingTab {
-  plugin: TyprPlugin;
-
-  constructor(app: App, plugin: TyprPlugin) {
-    super(app, plugin);
-    this.plugin = plugin;
-  }
-
-  display(): void {
-    const { containerEl } = this;
-    containerEl.empty();
-
-    containerEl.createEl("h2", { text: "Typr Autocompletion Settings" });
-
-    new Setting(containerEl)
-      .setName("Minimum word length")
-      .setDesc("Minimum number of characters before showing suggestions")
-      .addSlider((slider) =>
-        slider
-          .setLimits(1, 5, 1)
-          .setValue(this.plugin.settings.minWordLength)
-          .setDynamicTooltip()
-          .onChange(async (value) => {
-            this.plugin.settings.minWordLength = value;
-            // Update suggestor
-            this.plugin.suggestor.minChars = value;
-            await this.plugin.saveSettings();
-          })
-      );
-
-    new Setting(containerEl)
-      .setName("Maximum suggestions")
-      .setDesc("Maximum number of suggestions to show")
-      .addSlider((slider) =>
-        slider
-          .setLimits(1, 10, 1)
-          .setValue(this.plugin.settings.maxSuggestions)
-          .setDynamicTooltip()
-          .onChange(async (value) => {
-            this.plugin.settings.maxSuggestions = value;
-            // Update suggestor
-            this.plugin.suggestor.limit = value;
-            await this.plugin.saveSettings();
-          })
-      );
-
-    new Setting(containerEl)
-      .setName("Fuzzy matching")
-      .setDesc("Use fuzzy matching for suggestions")
-      .addToggle((toggle) =>
-        toggle
-          .setValue(this.plugin.settings.fuzzyMatching)
-          .onChange(async (value) => {
-            this.plugin.settings.fuzzyMatching = value;
-            // Update suggestor
-            this.plugin.suggestor.fuzzyMatching = value;
-            await this.plugin.saveSettings();
-          })
-      );
-
-    new Setting(containerEl)
-      .setName("Digit selection")
-      .setDesc("Allow faster selection via pressing digit keys")
-      .addToggle((toggle) =>
-        toggle
-          .setValue(this.plugin.settings.numberSelection)
-          .onChange(async (value) => {
-            this.plugin.settings.numberSelection = value;
-            // Update suggestor
-            this.plugin.suggestor.numberSelectionEnabled = value;
-            await this.plugin.saveSettings();
-          })
-      );
-
-    new Setting(containerEl)
-      .setName("Debounce time")
-      .setDesc(
-        "Time in milliseconds to wait after typing before showing suggestions"
-      )
-      .addSlider((slider) =>
-        slider
-          .setLimits(0, 100, 10)
-          .setValue(this.plugin.settings.debounceTime)
-          .setDynamicTooltip()
-          .onChange(async (value) => {
-            this.plugin.settings.debounceTime = value;
-            // Update suggestor
-            this.plugin.suggestor.debounceDelay = value;
-            await this.plugin.saveSettings();
-          })
-      );
   }
 }
